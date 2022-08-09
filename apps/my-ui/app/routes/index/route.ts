b1bbc9aac4c1e6580d29194e5d8239505162ca28
Ryan Tablada <ryan.tablada@gmail.com>
2022-08-08 21:12:38 -0500

    fix: redirect index to login

--- apps/my-ui/app/routes/index/route.ts
+++ apps/my-ui/app/routes/index/route.ts
@@ -1,12 +1,11 @@
-import Transition from '@ember/routing/-private/transition';
 import Route from '@ember/routing/route';
 import RouterService from '@ember/routing/router-service';
 import { service } from '@ember/service';
 
 export default class LoginRoute extends Route {
-  @service('router') declare router: RouterService;
+  @service() declare router: RouterService;
 
-  async beforeModel(transition: Transition) {
-    await this.router.transitionTo('login');
+  beforeModel() {
+    return this.router.transitionTo('login');
   }
 }
